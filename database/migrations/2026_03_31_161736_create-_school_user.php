a3a3a9b87094b067c07e8fdc9f96d3d84472172b
rahelaqambari <rahelaqambari@gmail.com>
2026-03-31 20:49:01 +0430

    changes

--- database/migrations/2026_03_31_161736_create-_school_user.php
+++ database/migrations/2026_03_31_161736_create-_school_user.php
@@ -11,10 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('payments', function (Blueprint $table) {
-            //
-            $table->decimal("amunt",12,2);
-        });
+        //
     }
 
     /**
@@ -22,8 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('payments', function (Blueprint $table) {
-            //
-        });
+        //
     }
 };
