d03e8ea7f9c91ad3266beaa937000d9712d3937f
rahelaqambari <rahelaqambari@gmail.com>
2026-04-09 21:20:24 +0430

    style

--- resources/views/livewire/feature.blade.php
+++ resources/views/livewire/feature.blade.php
@@ -1,11 +1,19 @@
 <div class="max-w-6xl mx-auto ">
     <div class="flex w-full flex-col mt-6 gap-4">
         <h1 class="text-xl md:text-4xl lg:text-5xl text-brand text-center font-serif">Sharifi High School</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!</p>
     </div>
     <div class="w-full grid grid-cols-3 gap-2" >
-        <div class="h-72 w-full col-span-2 rounded-lg shadow-[0px_0px_4px_rgba(0,0,0,0.4)] bg-white"></div>
-        <div class="h-72 w-full col-span-1 rounded-lg shadow-[0px_0px_4px_rgba(2,0,0,0.4)] bg-white"></div>
+        <div class=" w-full col-span-2 rounded-lg shadow-[0px_2px_6px_rgba(0,0,0,0.4)] bg-white">
+             <h1 class="text-xl md:text-2xl lg:text-3xl text-brand py-3 px-4 font-serif">About the School</h1>
+            <p class=" px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!</p>
+            <div class="grid w-full grid-cols-3 gap-3">
+                <div class="grid-cols-2 w-full">
+                    <h1 class="text-xl md:text-2xl lg:text-3xl text-brand text-center font-serif">Manager: Esmatullah Ghulami</h1>
+                </div>
+            </div>
+        </div>
+        <div class="h-72 w-full col-span-1 rounded-lg shadow-[0px_2px_6px_rgba(0,0,0,0.4)] bg-white"></div>
     </div>
     {{-- Do your work, then step back. --}}
 </div>
