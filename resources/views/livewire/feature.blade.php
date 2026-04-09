6cd5c890e7f44a52a0d92686b2ef2340b835b58f
rahelaqambari <rahelaqambari@gmail.com>
2026-04-09 21:31:22 +0430

    style

--- resources/views/livewire/feature.blade.php
+++ resources/views/livewire/feature.blade.php
@@ -8,9 +8,11 @@
              <h1 class="text-xl md:text-2xl lg:text-3xl text-brand py-3 px-4 font-serif">About the School</h1>
             <p class=" px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!</p>
             <div class="grid w-full grid-cols-3 gap-3">
-                <div class="grid-cols-2 w-full">
-                    <h1 class="text-xl md:text-2xl lg:text-3xl text-brand text-center font-serif">Manager: Esmatullah Ghulami</h1>
+                <div class="col-span-2 w-full">
+                    <h1 class="px-4 py-3 text-xl md:text-2xl lg:text-3xl text-brand font-serif">Manager: Esmatullah Ghulami</h1>
+                    <p class=" px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!Lorem ipsum dolor sit amet .</p>
                 </div>
+                    <img class="h-72 border border-4 mx-auto border-brand rounded-md w-72" src="images/Test1.jpg"/>
             </div>
         </div>
         <div class="h-72 w-full col-span-1 rounded-lg shadow-[0px_2px_6px_rgba(0,0,0,0.4)] bg-white"></div>
