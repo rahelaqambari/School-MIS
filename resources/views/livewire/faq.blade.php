62f673067fd188045e5683708e9f7e4e7a5608a4
rahelaqambari <rahelaqambari@gmail.com>
2026-04-11 21:16:40 +0430

    changes

--- resources/views/livewire/faq.blade.php
+++ resources/views/livewire/faq.blade.php
@@ -1,9 +1,17 @@
-<div class="max-w-6xl mx-auto felx flex-col text-center pt-4">
-    <h1 class="text-xl md:text-2xl lg:text-3xl text-brand py-3 px-4 font-serif">Frequently Ask Qestion</h1>
-    <p>The Thinges You Need To Know</p>
+<div class="max-w-6xl mx-auto felx flex-col text-center pt-4 bg-white mt-6">
+    <div class="w-full grid grid-cols-3 gap-2 justify-between " >
+        <div class="w-full col-span-1 rounded-lg ">
+            <h1 class="text-2xl md:text-4xl lg:text-7xl text-brand py-3 px-4 font-serif">Frequently Ask Qestion</h1>
+    <p class="font-meduim pt-6">The Thinges You Need To Know</p>
+        </div>
+    <div class=" w-full col-span-2 rounded-lg bg-white">
+         <h1 class="text-xl md:text-2xl lg:text-3xl text-brand px-4 font-serif">FAQ</h1>
     @foreach ($data as $q)
-        <h1 class="text-xl md:text-2xl lg:text-3xl text-brand py-3 px-4 font-serif">{{ $q[0]}}</h1>
+        <h1 class="border border-b border-0 border-b-2 border-black w-[55%] px-2 py-3">{{ $q[0]}}</h1>
         <button></button>
     @endforeach
+    </div>
+    </div>
     {{-- Care about people's approval and you will be their prisoner. --}}
 </div>
+{{-- <div class="w-full grid grid-cols-3 gap-2" > --}}
--- resources/views/livewire/our-team.blade.php
+++ resources/views/livewire/our-team.blade.php
@@ -1,15 +1,20 @@
 <div class="max-w-6xl mx-auto mt-8">
     <h1 class="px-4 py-3 text-center text-xl md:text-2xl lg:text-3xl text-brand font-serif">Our Team</h1>
     <div class="flex items-center justify-center gap-7">
-        <div class="bg-white shadow-[8px_2px_6px_rgba(0,0,0,0.4)] w-1/2 border border-2 border-brand flex flex-col items-center justify-center rounded-lg">
+        <div class="bg-white shadow-[4px_2px_6px_rgba(0,0,0,0.4)] w-1/3 border border-2 border-brand flex flex-col items-center justify-center rounded-lg">
                  <img class="h-52 p-4 mx-auto border-brand rounded-full w-52" src="images/me.jpg"/>
              <h1 class="px-4 py-2 text-center text-xl md:text-2xl lg:text-3xl text-brand font-serif">Rahela Qambari</h1>
-            <p class=" px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!</p>
+            <p class=" px-4 pb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!</p>
         </div>
-        <div class=" bg-white shadow-[8px_2px_6px_rgba(0,0,0,0.4)]  w-1/2 border border-2 border-brand flex flex-col items-center justify-center rounded-lg">
+        <div class=" bg-white shadow-[4px_2px_6px_rgba(0,0,0,0.4)]  w-1/3 border border-2 border-brand flex flex-col items-center justify-center rounded-lg">
                  <img class="h-52 p-4 mx-auto border-brand rounded-full w-52" src="images/Test1.jpg"/>
              <h1 class="px-4 py-2 text-center text-xl md:text-2xl lg:text-3xl text-brand font-serif">Asadullah Zamani</h1>
-            <p class=" px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!</p>
+            <p class=" px-4 pb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!</p>
+        </div>
+        <div class=" bg-white shadow-[4px_2px_6px_rgba(0,0,0,0.4)]  w-1/3 border border-2 border-brand flex flex-col items-center justify-center rounded-lg">
+                 <img class="h-52 p-4 mx-auto border-brand rounded-full w-52" src="images/Test1.jpg"/>
+             <h1 class="px-4 py-2 text-center text-xl md:text-2xl lg:text-3xl text-brand font-serif">Asadullah Zamani</h1>
+            <p class=" px-4 pb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque sed suscipit excepturi cum amet repellendus iste corporis beatae nesciunt praesentium sit ea id cupiditate nisi magni, laborum dolore ad!</p>
         </div>
     </div>
     {{-- In work, do what you enjoy. --}}
