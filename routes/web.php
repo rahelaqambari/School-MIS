579703b710a6c2e84e14051d9f5e360b0787dfd1
rahelaqambari <rahelaqambari@gmail.com>
2026-04-16 20:40:42 +0430

    route grouping

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+Route::prefix('dashbord')->middleware(['auth','verified'])->group(function(){
+    Route::view("employee",'livewire.employee.index');
+    Route::view("contract",'livewire.contract.index');
+    Route::view("school",'livewire.school.index');
+    Route::view("department",'livewire.department.index');
+    Route::view("designation",'livewire.designation.index');
+    Route::view("salary",'livewire.salary.index');
+    Route::view("payment",'livewire.payment.index');
+    Route::view("payroll",'livewire.payroll.index');
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
